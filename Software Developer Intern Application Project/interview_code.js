4dbbded7c3fa5dc87f503cbbed97d542d2d90dd9
leanardos1212 <67398132+leanardos1212@users.noreply.github.com>
2021-03-17 02:07:41 -0500

    Update interview_code.js

--- Software Developer Intern Application Project/interview_code.js
+++ Software Developer Intern Application Project/interview_code.js
@@ -6,54 +6,51 @@
 
 $(function(){
 	//if you haven't used jQuery before, the code in here runs when the page first loads
+
+
+	
+	
 	
 	//As a test, let's add some html to the document. We'll make a simple dropdown list.
 	//Just call the `InsertText` function with the id of the element you want to insert html inside
 	//and the content to add.
-	InsertText('mainContent', "<select><option>Option A</option><option>Option B</option></select>");
-	// InsertText('test',"<select><option>"+routes+"</option></select>")
+	InsertText('mainContent',"<select name = 'dropdown' id = 'selectId'><option></option></select>")
 
+	txt = ''
+
+	routeIdArray = []
+
+	for(x in routes)
+		{
+		txt += (routes[x].longName+' - ' +routes[x].shortName);
+		options = "<option>"+txt+"</option>"
+		$(' #selectId' ).append(options);
+		txt = '';
+		routeIdArray.push(routes[x].routeId);
+		}
+	
+		console.log(routeIdArray);
+	
+	
+	
+	
+
+
+
+
+
+	
 	//Also, as a test, let's take a look at some of the data from the json files. This will log it to your browsers
 	//javascript console. You can see the output by opening up the dev tools in your browser.
-	console.log(routes);
+	// console.log(routes);
 })
+
 
 //This is a helper function we're giving you to insert any html you want inside an element with the specified Id
 function InsertText(Id, Text){
 	var Element = document.getElementById(Id);
 	Element.innerHTML = Text;
 };
-
-// var $dropdown = $("#test");
-// $.each(result,function(){
-// 	$dropdown.append($("<option />").val(this.shortName.text(this.longName)))
-// })
-
-$.getJSON("/routes.json", function(result) {
-    for (var i = 0; i < result.length; i++) {
-        options += '<option value="' + result[i].ImageFolderID + '">' + result[i].Name + '</option>';
-    }
-});
-
-
-
-
-
-// let dropdown = $('#locality-dropdown');
-
-// dropdown.empty();
-
-// dropdown.append('<option selected="true" disabled>Choose State/Province</option>');
-// dropdown.prop('selectedIndex', 0);
-
-
-// // Populate dropdown with list of provinces
-// $.getJSON(routes, function (data) {
-//   $.each(data, function (key, entry) {
-//     dropdown.append($('<option></option>').attr('value', entry.shortName).text(entry.Longname));
-//   })
-// });
-
 
 
 
